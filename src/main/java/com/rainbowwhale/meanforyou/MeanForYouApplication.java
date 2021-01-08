2eae0a0fd4a50fe7d61e590541c4acd0349d0dd7
Tanya <thaiwan7@gmail.com>
2021-01-08 00:54:46 -0800

    First commit

---
+++ src/main/java/com/rainbowwhale/meanforyou/MeanForYouApplication.java
@@ -0,0 +1,13 @@
+package com.rainbowwhale.meanforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeanForYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeanForYouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rainbowwhale/meanforyou/MeanForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainbowwhale.meanforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeanForYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
